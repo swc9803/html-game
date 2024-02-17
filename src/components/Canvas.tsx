f2d9f74e39a8dcdfcbd06798bb0f9fdb7f988074
swc9803 <swc9803@gmail.com>
2024-02-17 21:34:00 +0900

    resize 이벤트 추가

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -4,12 +4,25 @@ const Canvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    if (!canvasRef.current) return;
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext('2d');
 
-    const ctx = canvasRef.current.getContext('2d');
+      const onResize = () => {
+        if (canvasRef.current) {
+          canvasRef.current.width = window.innerWidth;
+          canvasRef.current.height = window.innerHeight;
 
-    if (ctx) {
-      ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+          if (ctx) {
+            ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+          }
+        }
+      };
+
+      window.addEventListener('resize', onResize);
+      onResize();
+      return () => {
+        window.removeEventListener('resize', onResize);
+      };
     }
   }, [canvasRef.current]);
 
