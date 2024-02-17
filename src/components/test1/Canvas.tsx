be9a7d0fb2a3f0e48608f9cfdd19a57b3bc0a2db
swc9803 <swc9803@gmail.com>
2024-02-17 23:29:38 +0900

    border animate test

---
+++ src/components/test1/Canvas.tsx
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react';
+
+const Canvas: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  let raf: number;
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext('2d');
+      let x = 50;
+      let y = 50;
+      let xSpeed = 2;
+      let ySpeed = 2;
+
+      const drawCircle = () => {
+        ctx!.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
+        ctx!.beginPath();
+        ctx!.arc(x, y, 30, 0, 2 * Math.PI);
+        ctx!.fillStyle = '#0095DD';
+        ctx!.fill();
+        ctx!.closePath();
+
+        x += xSpeed;
+        y += ySpeed;
+
+        if (x + 30 > canvasRef.current!.width || x - 30 < 0) {
+          xSpeed = -xSpeed;
+        }
+
+        if (y + 30 > canvasRef.current!.height || y - 30 < 0) {
+          ySpeed = -ySpeed;
+        }
+
+        raf = requestAnimationFrame(drawCircle);
+      };
+
+      drawCircle();
+
+      return () => {
+        cancelAnimationFrame(raf);
+      };
+    }
+  }, [canvasRef.current]);
+
+  const onResize = () => {
+    if (canvasRef.current) {
+      canvasRef.current.width = window.innerWidth;
+      canvasRef.current.height = window.innerHeight;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize);
+    onResize();
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />;
+};
+
+export default Canvas;
